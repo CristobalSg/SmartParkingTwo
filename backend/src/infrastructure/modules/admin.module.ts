df714c12e14590bcb60c537f41397d7e3d10395b
Benjamin Espinoza <bespinoza2021@alu.uct.cl>
2025-09-05 03:24:32 -0400

    feat: :art: Implementacion de post
    Se implementa la creacion de un administrador añadiendo los modulos y su cintructor en la capa de controlladores

--- backend/src/infrastructure/modules/admin.module.ts
+++ backend/src/infrastructure/modules/admin.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from '../../presentation/controllers/AdminController';
 import { AdminLoginUseCase } from '../../application/use-cases/adminUseCases/AdminLoginUseCase';
+import { CreateAdminUseCase } from '../../application/use-cases/adminUseCases/CreateAdminUseCase';
 import { PrismaModule } from '../database/prisma.module';
+import { TenantModule } from '../modules/tenant.module';
 import { PrismaAdminRepository } from '../repositories/PrismaAdminRepository';
 import { TenantContext } from '../context/TenantContext';
 
 export const ADMIN_REPOSITORY_TOKEN = 'ADMIN_REPOSITORY_TOKEN';
 export const ADMIN_LOGIN_USE_CASE_TOKEN = 'ADMIN_LOGIN_USE_CASE_TOKEN';
-
+export const CREATE_ADMIN_USE_CASE_TOKEN = 'CREATE_ADMIN_USE_CASE_TOKEN';
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, TenantModule],
     controllers: [AdminController],
     providers: [
         // Repositorio de administradores
@@ -25,7 +27,16 @@ export const ADMIN_LOGIN_USE_CASE_TOKEN = 'ADMIN_LOGIN_USE_CASE_TOKEN';
             },
             inject: [ADMIN_REPOSITORY_TOKEN, TenantContext],
         },
+        //Caso de uso crear admin
+        {
+            provide: CREATE_ADMIN_USE_CASE_TOKEN,
+            useFactory: (adminRepository, tenantContext) => {
+                return new CreateAdminUseCase(adminRepository, tenantContext);
+            },
+            inject: [ADMIN_REPOSITORY_TOKEN, TenantContext],
+
+        },
     ],
-    exports: [ADMIN_REPOSITORY_TOKEN, ADMIN_LOGIN_USE_CASE_TOKEN],
+    exports: [ADMIN_REPOSITORY_TOKEN, ADMIN_LOGIN_USE_CASE_TOKEN, CREATE_ADMIN_USE_CASE_TOKEN],
 })
 export class AdminModule { }
