1951a46ffcc0b5b1a19ba7be4135700dd678e54e
Benjamin Espinoza <bespinoza2021@alu.uct.cl>
2025-09-07 01:59:36 -0300

    feat: :art: Se implemento el actualizar
    Se creo el caso de uso de actualizar para admin y su modulo con el controlador y inyecciones de dependencias

--- backend/src/infrastructure/modules/admin.module.ts
+++ backend/src/infrastructure/modules/admin.module.ts
@@ -8,13 +8,15 @@ import { PrismaAdminRepository } from '../repositories/PrismaAdminRepository';
 import { TenantContext } from '../context/TenantContext';
 import { GetAllAdminsUseCase } from '../../application/use-cases/adminUseCases/GetAllAdminUseCase';
 import { GetAdminByIdUseCase } from '../../application/use-cases/adminUseCases/GetAdminByIdUseCase';
-
+import { UpdateAdminUseCase } from '../../application/use-cases/adminUseCases/UpdateAdminUseCase';
 
 export const ADMIN_REPOSITORY_TOKEN = 'ADMIN_REPOSITORY_TOKEN';
 export const ADMIN_LOGIN_USE_CASE_TOKEN = 'ADMIN_LOGIN_USE_CASE_TOKEN';
 export const CREATE_ADMIN_USE_CASE_TOKEN = 'CREATE_ADMIN_USE_CASE_TOKEN';
 export const GET_ALL_ADMINS_USE_CASE_TOKEN = 'GET_ALL_ADMINS_USE_CASE_TOKEN';
-export const GET_ADMIN_BY_ID_USE_CASE_TOKEN = 'GET_ADMIN_BY_ID_USE_CASE_TOKEN'
+export const GET_ADMIN_BY_ID_USE_CASE_TOKEN = 'GET_ADMIN_BY_ID_USE_CASE_TOKEN';
+export const UPDATE_ADMIN_USE_CASE_TOKEN = 'UPDATE_ADMIN_USE_CASE_TOKEN'
+
 @Module({
     imports: [PrismaModule, TenantModule],
     controllers: [AdminController],
@@ -60,13 +62,24 @@ export const GET_ADMIN_BY_ID_USE_CASE_TOKEN = 'GET_ADMIN_BY_ID_USE_CASE_TOKEN'
             inject: [ADMIN_REPOSITORY_TOKEN, TenantContext],
 
         },
+
+        //Caso de uso actualizar admin
+        {
+            provide: UPDATE_ADMIN_USE_CASE_TOKEN,
+            useFactory: (adminRepository, tenantContext) => {
+                return new UpdateAdminUseCase(adminRepository, tenantContext);
+            },
+            inject: [ADMIN_REPOSITORY_TOKEN, TenantContext],
+
+        },
     ],
     exports: [
         ADMIN_REPOSITORY_TOKEN,
         ADMIN_LOGIN_USE_CASE_TOKEN,
         CREATE_ADMIN_USE_CASE_TOKEN,
         GET_ALL_ADMINS_USE_CASE_TOKEN,
-        GET_ADMIN_BY_ID_USE_CASE_TOKEN
+        GET_ADMIN_BY_ID_USE_CASE_TOKEN,
+        UPDATE_ADMIN_USE_CASE_TOKEN
     ],
 })
 export class AdminModule { }
