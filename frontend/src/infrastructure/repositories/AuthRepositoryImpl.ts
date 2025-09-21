e93560e221b08078ff7970e7485ef39a067946de
pabloantivil <pabloantivil16@gmail.com>
2025-09-21 17:06:50 -0300

    refactor: eliminar archivos obsoletos por parte del front

--- frontend/src/infrastructure/repositories/AuthRepositoryImpl.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { AuthRepository } from "../../domain/repositories/AuthRepository";
-
-export class AuthRepositoryImpl implements AuthRepository {
-  async login(email: string, password: string): Promise<void> {
-    const response = await axios.post("/api/login", { email, password });
-    return response.data;
-  }
-}
