09e2bb0897f7baa40d4f5c4898be014ab0e7bef0
CristobalSg <cristobal.sandoval2022@alu.uct.cl>
2025-09-22 18:37:55 -0300

    chore(tailwind): establecer base de arquitectura clean para CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,26 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        // tailwindcss-animate trae varias animaciones listas
+        // tailwindcss-animate
+      },
+      colors: {
+        primary: {
+          light: "#6366f1", // indigo-500
+          dark: "#7e22ce",  // purple-700
+        },
+        secondary: {
+          light: "#4b5563", // gray-600
+          dark: "#111827",  // gray-900
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      screens: {
+        sm: "640px",  // móvil
+        md: "768px",  // tablet
+        lg: "1024px", // laptop/PC
+        xl: "1280px", // desktop grande
       },
     },
   },
